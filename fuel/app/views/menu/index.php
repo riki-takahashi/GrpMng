531ecebfadf71aeac91d2906660b5f5fa413de1e
i-suzuki <i-suzuki@rikisys.co.jp>
2014-08-05 12:05:52 +0900

    2014/08/05　iccha
    メニュー項目追加 #9 #10

--- fuel/app/views/menu/index.php
+++ fuel/app/views/menu/index.php
@@ -1,10 +1,12 @@
 <dl>
 	<dt>案件管理</dt>
 	<dd><?php echo Html::anchor('project/search', '案件情報'); ?></dd>
+	<dd><?php echo Html::anchor('assign', '社員アサイン状況'); ?></dd>
 	<br>
 	<dt>売上管理</dt>
 	<dd><?php echo Html::anchor('sales/term', '売上期間情報'); ?></dd>
 	<dd><?php echo Html::anchor('sales/target/search', '売上目標情報'); ?></dd>
+	<dd><?php echo Html::anchor('sales/achievement/search', '売上ポイント集計'); ?></dd>
 	<br>
 	<dt>マスタメンテ</dt>
 	<dd><?php echo Html::anchor('employee', '社員マスタ'); ?></dd>
