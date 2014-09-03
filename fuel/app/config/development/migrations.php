ba9dca688e8c78bae183ab37a6cc5786037743fb
i-suzuki <i-suzuki@rikisys.co.jp>
2014-09-03 19:34:06 +0900

    2014/09/03　iccha
    マイグレーションファイルは共有するようにgit ignoreから除外した

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -14,6 +14,13 @@ return array(
 				5 => '006_add_is_mng_flag_to_employees',
 				6 => '007_create_projectmembers',
 				7 => '008_create_sales_terms',
+				8 => '009_create_sales_targets',
+				9 => '010_create_sales_results',
+				10 => '011_change_field_type_to_timestamp_in_sales_targets',
+				11 => '012_change_field_type_to_timestamp_in_sales_results',
+				12 => '013_change_field_auto_increment_in_sales_targets',
+				13 => '014_change_field_auto_increment_in_sales_results',
+				14 => '015_create_users',
 			),
 		),
 		'module' => 
