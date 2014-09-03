8c7332654ecf99f3c79c222cfb0e7c15345928af
i-suzuki <i-suzuki@rikisys.co.jp>
2014-09-03 15:43:21 +0900

    2014/09/03　iccha
    エラーが発生したときのためにエラー画面を用意 #15

---
+++ fuel/app/views/errors/production.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+        <meta http-equiv="refresh" content="15;URL=/GrpMng/">
+        <title>FuelPHP Framework</title>
+	<style type="text/css">
+		* { margin: 0; padding: 0; }
+		body { background-color: #FFF; font-family: sans-serif; font-size: 16px; line-height: 20px; margin: 40px; }
+		#wrapper { padding: 30px; background: #fff; color: #333; margin: 0 auto; width: 800px; }
+		h1 { color: #CCC; font-size: 55px; padding: 0 0 25px; line-height: 1em; }
+		.intro { font-size: 22px; line-height: 30px; font-family: georgia, serif; color: #555; padding: 29px 0 20px; border-top: 1px solid #CCC; }
+		p { margin: 0 0 15px; line-height: 22px;}
+	</style>
+</head>
+<body>
+	<div id="wrapper">
+		<h1>システム内で問題発生</h1>
+		<p class="intro">売上管理システム内で想定外のエラーが発生しました。<br>システム管理者にご連絡ください。</p>
+		<p class="intro">１５秒後にログイン画面またはメニュー画面に戻ります。</p>
+                <p style="text-align:center">画面が戻らない時は下記リンクをクリックしてください。</p>
+                <p style="text-align:center"><a href="/GrpMng/">戻る/</a></p>                
+	</div>
+</body>
+</html>
