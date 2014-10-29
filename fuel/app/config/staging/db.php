105beb1117ca8438fbeefadb346062aeb2276c7c
i-suzuki <i-suzuki@rikisys.co.jp>
2014-10-29 17:53:23 +0900

    2014/10/29　iccha #12
    db.iniのステージング環境修正

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -4,11 +4,23 @@
  */
 
 return array(
-	'default' => array(
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_staging',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
-		),
-	),
+        // MySQLi ドライバの設定 ステージング環境に合わせて設定
+        'default' => array(
+            'type'           => 'mysqli',
+            'connection'     => array(
+                'hostname'       => '127.0.0.1',
+                'port'           => '3306',
+                'database'       => 'grpmngtest',
+                'username'       => 'grpmngtest',
+                'password'       => '',
+                'persistent'     => false,
+                'compress'       => false,
+            ),
+            'identifier'     => '`',
+            'table_prefix'   => '',
+            'charset'        => 'utf8',
+            'enable_cache'   => true,
+            'profiling'      => false,
+            'readonly'       => false,
+        ),    
 );
